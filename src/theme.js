7c21c89db767c849240b678ff27468f6b196b4ea
Taerin <85108615+kimtaerin66@users.noreply.github.com>
2022-05-30 23:25:00 +0900

    Create theme.js

---
+++ src/theme.js
@@ -0,0 +1,17 @@
+// theme.js
+import { DefaultTheme } from 'styled-components';
+
+export const darktheme = {
+    bgColor : "black",
+    textColor : "white",
+    blue : "#04aaff",
+    red : "tomato"
+};
+
+
+export const lighttheme = {
+    bgColor : "white",
+    textColor : "#333",
+    blue : "#04aaff",
+    red : "tomato"
+};
